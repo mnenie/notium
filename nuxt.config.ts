9fdd3e526bd06262814150790bf8d8f736284d6c
mnenie <1alexpeshkov@gmail.com>
2024-03-25 18:30:57 +0300

    add: shadcn-ui + configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  shadcn: {
+    prefix: 'Ui',
+    componentDir: './components/ui'
+  }
 })
