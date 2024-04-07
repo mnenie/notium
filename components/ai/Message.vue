8d7b9cdf04135fc5cc74329b4cfdcd6df25cd8d3
mnenie <121057011+mneniee@users.noreply.github.com>
2024-04-07 22:32:12 +0300

    fix: ai page + skeleton

--- components/ai/Message.vue
+++ components/ai/Message.vue
@@ -13,7 +13,7 @@ onMounted(() => {
 <template>
   <div v-if="!isSkeleton" class="relative flex w-full flex-col items-center">
     <div class="relative w-full">
-      <UiTextarea placeholder="Tell NotiumAI a little bit about yourself" class="h-[46px] resize-none" />
+      <UiTextarea placeholder="Tell NotiumAI a little bit about yourself" class="h-[41px] resize-none" />
       <UiButton variant="ghost" class="absolute right-[3px] top-1/2 -translate-y-1/2 px-2">
         <ArrowBigUpDash color="rgb(82 82 91 / 0.9)" />
       </UiButton>
@@ -23,7 +23,7 @@ onMounted(() => {
     </span>
   </div>
   <div v-else class="flex flex-col w-full items-center">
-    <UiSkeleton class="h-12 w-full" />
+    <UiSkeleton class="h-[41px] w-full" />
     <UiSkeleton class="h-4 w-[250px] my-1" />
   </div>
 </template>
