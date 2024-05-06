3d0278e8ecab8e74689890d014cbfd26b729c126
mnenie <1alexpeshkov@gmail.com>
2024-05-07 01:27:09 +0300

    fix: ai model

--- app.vue
+++ app.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { Toaster } from '~/components/ui/sonner';
+const cacheList = ref(['ai', 'login', 'registration'])
 </script>
 
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <NuxtPage :keepalive="{include: cacheList, max:20}" />
     <Toaster />
   </NuxtLayout>
 </template>
