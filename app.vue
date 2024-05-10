f5a1f8a900b1d448c10d7f40d806d5de383fb110
mnenie <1alexpeshkov@gmail.com>
2024-05-10 14:08:02 +0300

    update: seo optimization

--- app.vue
+++ app.vue
@@ -3,24 +3,10 @@ import { useWindowSize } from '@vueuse/core';
 import { Toaster } from '~/components/ui/sonner';
 
 const cacheList = ref(['ai', 'login', 'registration']);
-const { width } = useWindowSize();
+const { width } = useWindowSize()
 </script>
 
 <template>
-  <Head>
-    <Title>Notium - smart cloud app for your notes</Title>
-    <Meta charset="utf-8" />
-    <Meta name="title" content="Notium - smart cloud app for your notes" />
-    <Meta name="og:title" content="Notium - smart cloud app for your notes" />
-    <Meta
-      name="description"
-      content="Notium is a cloud app allowing users to create and store notes based on their priorities, also using the built-in AI, assistant NotiumAI"
-    />
-    <Meta
-      name="og:description"
-      content="Notium is a cloud app allowing users to create and store notes based on their priorities, also using the built-in AI, assistant NotiumAI"
-    />
-  </Head>
   <NuxtLayout v-if="width >= 1120">
     <NuxtPage :keepalive="{ include: cacheList, max: 20 }" />
     <Toaster />
